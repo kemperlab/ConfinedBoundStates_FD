2a15f06f89a8d8bf0e2ff07aec6e3621a4f67572
Heba Labib <116379784+hlabib9@users.noreply.github.com>
2025-08-22 04:00:11 -0700

    changed the parameter relaxation function

--- fd-conf-ising.py
+++ fd-conf-ising.py
@@ -52,11 +52,12 @@ def par_relax(initial_par, final_par, t, t0, tau):
         NdArray of parameter evolving over time-axis with preferred direction
     
     '''
+
     if final_par > 0:
 
         if (initial_par-final_par) < 0:
 
-            return np.heaviside(t0-t, t) * initial_par + np.heaviside(t-t0, 0) * ((final_par-initial_par) * np.exp(-((t-t0)**2)/(tau*20)) +initial_par)
+            return np.heaviside(t0-t, t) * initial_par + np.heaviside(t-t0, 0) * ((final_par-initial_par) * np.exp(-((t-t0))/(tau)) +initial_par)
         
         else:
 
@@ -66,7 +67,8 @@ def par_relax(initial_par, final_par, t, t0, tau):
         return np.heaviside(t-t0, 0)* (final_par * np.exp(-(t-t0)/tau)+ initial_par) + np.heaviside(t0-t, 0) * initial_par
 
 
-def fd_evolve(N, time_pars, h_pars, c1, probe_pars, site):
+
+def fd_evolve(N, time_pars, h_pars, c1, probe_pars, site, TV = True, TD = True):
 
     """
     
@@ -95,21 +97,28 @@ def fd_evolve(N, time_pars, h_pars, c1, probe_pars, site):
     """
 
     PauliSparse = op_sparse(N, probe_pars['opp'])
-
+    hl_max = c1['h_l']
     ct_l = par_relax(h_pars['h_l0'], c1['h_l'], time_pars['tax'], time_pars['t0'], time_pars['tau'])
 
     js = np.zeros(probe_pars['j_points']+1)
     js[0] = 5
     js[1:]=np.linspace(15,50,probe_pars['j_points'])#np.geomspace(time_pars['t0'], time_pars['tmax']-5, probe_pars['j_points'])
+    H1 = sparse_mat(N=N, coeffs=c1, model=h_pars['model'])
+    
+    #evals, psi0_p = scilasp.eigsh(H1,1)
+
     psi0_p = np.zeros(2 ** N, dtype=complex)
     psi0_p[0] = 1
+
     tax = time_pars['tax']
 
     response_array = np.ones((N, len(js), time_pars['nt']), dtype= 'complex')
     hit_arr = np.zeros((len(js), time_pars['nt']), dtype = complex)
     psi = np.zeros((nt, 2**N), dtype = 'complex')
     psi[0,:] = psi0_p
-    
+
+     
+  
     for j in range(len(js)):
 
         d_h = probes_f(probe_pars['sigma'], js[j], probe_pars['amp'], probe_pars['w'], tax)
@@ -117,46 +126,61 @@ def fd_evolve(N, time_pars, h_pars, c1, probe_pars, site):
         hit_arr[j,:] = d_h.probe(probe_pars['mapping'])
         hit_arr[0,:] = 0
 
-        psi = np.zeros((nt, 2**N), dtype = 'complex')
         psi[0,:] = psi0_p
-        
-        for i in range(time_pars['nt']-1):
           
-            c1['h_l'] = ct_l[i]
+        for i in range(time_pars['nt']-1):
+
+            if TD == True:
+                c1['h_l'] = ct_l[i]
            
-            H1 = sparse_mat(N=N, coeffs=c1, model=h_pars['model'])    
+                H1 = sparse_mat(N=N, coeffs=c1, model=h_pars['model'])    
             
             for n in range(N):
                 
-                response_array[n, j, i+1] = np.conj(np.transpose(PauliSparse[n] @psi[i,:])) @  psi[i,:]     
+                response_array[n, j, i+1] = np.conj(psi[i,:]) @ PauliSparse[n] @  psi[i,:]     
 
             psi[i+1,:] = scilasp.expm_multiply(-1.j * (H1 + hit_arr[j,i] * PauliSparse[site]) * time_pars['dt'], psi[i,:])
 
+    if TV == True:
+       
+        np.savez(f'response_FD_TV_{hl_max}.npz', arr1 = response_array, arr2 = js, arr3 = hit_arr, arr4 = ct_l)
+    
+    else:
+        
+        np.savez(f'response_FD_FV_S{hl_max}_sigma{probe_pars['sigma']}.npz', arr1 = response_array, arr2 = js, arr3 = hit_arr, arr4 = ct_l)
+    
     return response_array, js, hit_arr, ct_l
 
-N = 10
+TV = True
+N = 12
 nt = 2000
 tmax = 80
 tax = np.linspace(0, tmax, nt)
 dt = tax[1]-tax[0]
 t0 = 7
 
-tau = 60
+tau = 35 
 sigma = 0.2
-amp = 0.1
-w = 4
-h_l0 = 0
-h_l = 0.4
-nj = 100
-site = 0
+amp = 0.01
 
+w = 2.2
+h_l0 = 0
+h_l = -0.4
+nj = 100
+site = 5
+
+
+mapping = 'GAUSSIAN'
 time_pars = {'tax': tax, 'dt': dt, 'nt': nt, 't0': t0, 'tau': tau, 'tmax': tmax}
-probe_pars = {'j_points': nj,'sigma': sigma, 'amp': amp, 'w': w, 'mapping': 'GAUSSIAN', 'opp': 'Z', 'site': 0}
+probe_pars = {'j_points': nj,'sigma': sigma, 'amp': amp, 'w': w, 'mapping': mapping, 'opp': 'Z', 'site': 5}
 h_pars = {'model':'LF', 'h_l0': h_l0}
 c1 = {'J':1, 'h_t':0.25, 'h_l':h_l}
 
-A, js, hit_arr,ct_l = fd_evolve(N, time_pars, h_pars, c1, probe_pars, site)
-np.savez('response_FD_NEW.npz', arr1 = A, arr2 = js, arr3 = hit_arr, arr4 = ct_l)
+if h_l < 0:
+    TV = False
+
+A, js, hit_arr,ct_l = fd_evolve(N, time_pars, h_pars, c1, probe_pars, site,TV)
+
 
 
 
@@ -223,13 +247,13 @@ def f_derivatives(response, j_points, N, tax, hit_arr, damping_power):
 
             # response_avg[n,j,:] = response[n,j,:]-np.average(response[n,j,:], axis = 0)
             A_damped[n,j,:] = damper*(response[n,j,:]-response[n,0,:])
-            A_ft[n,j,:] = np.fft.fftshift(np.fft.fft(A_damped[n,j,:]))
+            A_ft[n,j,:] = np.fft.fftshift(np.fft.fft(A_damped[n,j,:], norm='ortho')) 
 
 
     
     for j in range(1,len(j_points)):
         
-        hofw[j, :] = np.fft.fftshift(np.fft.fft(hit_arr[j, :]))
+        hofw[j, :] = np.fft.fftshift(np.fft.fft(hit_arr[j, :], norm='ortho'))
         hofw[j, :] /= max(abs(hofw[j, :]))
   
 
@@ -256,6 +280,10 @@ def f_derivatives(response, j_points, N, tax, hit_arr, damping_power):
             
     return wax, A_ft, A_damped, ratio, hofw  
 
-wax, a_FT, A_damped, corr_FD, hofw = f_derivatives(A, js, N, tax, hit_arr, 10)
+wax, a_FT, A_damped, corr_FD, hofw = f_derivatives(A, js, N, tax, hit_arr, 40)
 
-np.savez('correlations_FD.npz', arr1 = a_FT, arr2 = corr_FD, arr3 = wax, arr4 = hofw, arr5 = A_damped)
+
+if mapping == 'SELECTIVE':
+    np.savez(f'correlations_S_FD_sigma{probe_pars['sigma']}.npz', arr1 = a_FT, arr2 = corr_FD, arr3 = wax, arr4 = hofw, arr5 = A_damped)
+else:
+    np.savez('correlations_FD.npz', arr1 = a_FT, arr2 = corr_FD, arr3 = wax, arr4 = hofw, arr5 = A_damped)
